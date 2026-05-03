c26623ea8413fd0eeaf4700e9585d1267a9c5c4f
Secret-30 <samruddhiarmy@gmail.com>
2026-05-03 19:23:59 +0530

    Add styles for range input and labels
    Added styles for range input and labels.

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ playPauseBtn.addEventListener('click', () => {
     }
     isPlaying = !isPlaying;
 });
+input[type=range] {
+    width: 100%;
+    margin: 15px 0;
+    cursor: pointer;
+}
+/* Style the labels */
+label { font-size: 12px; color: #555; }
